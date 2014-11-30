ff6f47179f694cffbb33da61d6f2aaae0d37d199
LiquidHelium <andydjackson@gmail.com>
2014-11-30 16:34:35 +0000

    better symbol not found error handling

--- src/AST/Symbol.hpp
+++ src/AST/Symbol.hpp
@@ -16,18 +16,24 @@ public:
 	virtual DataType type() const { return DataType::Symbol; }
 
 	virtual GTPtr return_type(Enviroment &env) const {
-		return env.value_map[val].type;
+		assert_var_exists(env);
+		auto found_value = env.value_map[val];
+		return found_value.type;
 	}
 
 	GenericValue to_value(Enviroment &env, llvm::IRBuilder<> &builder) {
-		if (env.value_map.find(val) == env.value_map.end()) {
-			std::cerr << "Could not find symbol: " << val << std::endl;
-			throw;
-			exit(1);
-		}
+		assert_var_exists(env);
 		auto found_value = env.value_map[val];
 		return found_value;
 	}
 
 	virtual std::string as_string() const { return val; }
+
+	void assert_var_exists(Enviroment &env) const {
+		if (env.value_map.find(val) == env.value_map.end()) {
+			std::cerr << "Could not find symbol: " << val << std::endl;
+			throw;
+			exit(1);
+		}
+	}
 };
